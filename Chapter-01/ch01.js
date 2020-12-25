803d7c7ad8fc918ee3ac5238710093eb4fc816ca
Shubham Gupta <shugupta@microsoft.com>
2020-12-25 23:32:12 +0530

    Update ch01.js

--- Chapter-01/ch01.js
+++ Chapter-01/ch01.js
@@ -4,3 +4,15 @@ div.onclick = function () {
   alert("This is the box #" + i);
 }
 container.appendChild(div);
+
+// on clicking on each block 5 will appear since value of i os global 5
+// using let will block off the scope of i
+
+// the scope of i is protected with let
+
+// # Function Parameters
+const logCompliment = function (name) {
+	alert(`${name} is awesome!`);
+}
+
+logCompliment('Shubham');
